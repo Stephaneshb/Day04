5cc89be46c88e82f737c77e98cba7ac6f54934ca
Stephaneshb <stephane.shalaby@epitech.digital>
2024-10-03 14:59:47 +0200

    Create ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var para = document.getElementsByTagName('p');
+    for (var i = 0; i < para.length; i++) {
+        para[i].innerHTML = i; 
+    }});
